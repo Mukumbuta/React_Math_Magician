2078d60cc52caa7656cafce776950716588484d7
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-11-30 13:22:20 +0500

    add a re-usable input/display component

---
+++ src/components/Display.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line react/prefer-stateless-function
+export class Display extends Component {
+  render() {
+    return (
+      <div>
+        <input type="text" className="display" placeholder="0" />
+      </div>
+    );
+  }
+}
+
+export default Display;
