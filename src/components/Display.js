ba38045ac864f6da0279d4b05cf18fd629113a37
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-01 14:53:40 +0500

    pass the result of the arithmetic operation to display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,14 +1,17 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class Display extends Component {
   render() {
+    const { value } = this.props;
     return (
       <div>
-        <input type="text" className="display" placeholder="0" />
+        <input type="text" className="display" placeholder={value} />
       </div>
     );
   }
 }
 
+Display.propTypes = { value: PropTypes.string.isRequired };
 export default Display;
