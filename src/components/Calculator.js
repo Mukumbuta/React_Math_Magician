ce6001b32dce9bc6c240a6242d5e11f489e8e4b8
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-01 21:15:09 +0500

    refactored Calculator component to use React Hooks

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,64 +1,60 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Button from './Button';
 import Screen from './Display';
 import calculate from '../logic/calculate';
 
-// eslint-disable-next-line react/prefer-stateless-function
-export default class Calculator extends Component {
-  constructor() {
-    super();
-    this.handleClick = this.handleClick.bind(this);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
-  }
+const Calculator = () => {
+  const [CalData, setCalData] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-  handleClick(e) {
+  const handleClick = (e) => {
     // eslint-disable-next-line react/no-access-state-in-setstate
-    this.setState(calculate(this.state, e.target.textContent));
-  }
+    setCalData(calculate(CalData, e.target.textContent));
+  };
 
-  render() {
-    const { total } = this.state;
-    return (
-      <div className="calc-frame">
-        <div className="main-calc">
-          <Screen value={total || '0'} />
-          <div className="btn-row">
-            <Button handleClick={this.handleClick} label="AC" />
-            <Button handleClick={this.handleClick} label="+/-" />
-            <Button handleClick={this.handleClick} label="%" />
-            <Button handleClick={this.handleClick} label="÷" />
+  const { total } = CalData;
+
+  return (
+    <div className="calc-frame">
+      <div className="main-calc">
+        <Screen value={total || '0'} />
+        <div className="btn-row">
+          <Button handleClick={handleClick} label="AC" />
+          <Button handleClick={handleClick} label="+/-" />
+          <Button handleClick={handleClick} label="%" />
+          <Button handleClick={handleClick} label="÷" />
+        </div>
+        <div className="btn-row">
+          <Button handleClick={handleClick} label="7" />
+          <Button handleClick={handleClick} label="8" />
+          <Button handleClick={handleClick} label="9" />
+          <Button handleClick={handleClick} label="×" />
+        </div>
+        <div className="btn-row">
+          <Button handleClick={handleClick} label="4" />
+          <Button handleClick={handleClick} label="5" />
+          <Button handleClick={handleClick} label="6" />
+          <Button handleClick={handleClick} label="+" />
+        </div>
+        <div className="btn-row">
+          <Button handleClick={handleClick} label="1" />
+          <Button handleClick={handleClick} label="2" />
+          <Button handleClick={handleClick} label="3" />
+          <Button handleClick={handleClick} label="-" />
+        </div>
+        <div className="last-btn-row btn-row">
+          <div className="zero-btn">
+            <Button handleClick={handleClick} label="0" />
           </div>
-          <div className="btn-row">
-            <Button handleClick={this.handleClick} label="7" />
-            <Button handleClick={this.handleClick} label="8" />
-            <Button handleClick={this.handleClick} label="9" />
-            <Button handleClick={this.handleClick} label="×" />
-          </div>
-          <div className="btn-row">
-            <Button handleClick={this.handleClick} label="4" />
-            <Button handleClick={this.handleClick} label="5" />
-            <Button handleClick={this.handleClick} label="6" />
-            <Button handleClick={this.handleClick} label="+" />
-          </div>
-          <div className="btn-row">
-            <Button handleClick={this.handleClick} label="1" />
-            <Button handleClick={this.handleClick} label="2" />
-            <Button handleClick={this.handleClick} label="3" />
-            <Button handleClick={this.handleClick} label="-" />
-          </div>
-          <div className="last-btn-row btn-row">
-            <div className="zero-btn">
-              <Button handleClick={this.handleClick} label="0" />
-            </div>
-            <Button handleClick={this.handleClick} label="." />
-            <Button handleClick={this.handleClick} label="=" />
-          </div>
+          <Button handleClick={handleClick} label="." />
+          <Button handleClick={handleClick} label="=" />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default Calculator;
