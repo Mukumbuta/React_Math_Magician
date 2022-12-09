d5f927b51084840114d2f095b56bf8340c6db0fc
Mukumbuta <mukumbutasimasiku@gmail.com>
2022-12-09 14:10:36 +0500

    add favorite quotes page

---
+++ src/routes/Quotes.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Quotes = () => (
+  <div className="quotes">
+    <p>
+      Mathematics is not about numbers, equations, compositions,
+      or algorithms: it is about understanding. - William Paul Thurston
+    </p>
+  </div>
+);
+
+export default Quotes;
